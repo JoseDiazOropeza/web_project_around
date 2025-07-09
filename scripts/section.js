ffa3d2eb817165841dc642a9c3d509272140e793
Jose Diaz Oropeza <josediazoropeza@gmail.com>
2025-07-09 13:52:40 -0600

    Crear la clase Section.js

---
+++ scripts/section.js
@@ -0,0 +1,20 @@
+// Clase Section para renderizar listas de elementos en un contenedor
+export default class Section {
+  constructor({ items, renderer }, containerSelector) {
+    this._items = items;
+    this._renderer = renderer;
+    this._container = document.querySelector(containerSelector);
+  }
+
+  // Renderiza todos los elementos usando la función renderer
+  renderItems() {
+    this._items.forEach(item => {
+      this._renderer(item);
+    });
+  }
+
+  // Agrega un elemento del DOM al contenedor
+  addItem(element) {
+    this._container.prepend(element);
+  }
+}
